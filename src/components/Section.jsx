e2b71ef1c3904ae2f97c0d31fbcbc02d876cd0c1
starshipog <chayewhitaker14@gmail.com>
2023-11-23 16:42:22 -0600

    updated main and section files

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,38 +1,13 @@
-import Footer from './components/Footer';
-import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-
-export default function Section() {
-    const linkStyle = { border: '1px black', padding: '5px' };
-  
-    return (
-      <header className="main-header-menu">
-        <section
-          style={{
-            display: 'flex',
-            fontFamily: 'helvetica',
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-            justifyContent: 'flex-start',
-          }}
-        >
+export default function Pages({ children, ...props }) {
+  return (
+    <li
+      className="list-group-item pt-4 d-flex justify-content-center align-items-center"
+      {...props}
+    >
+      {children}
+    </li>
+  );
+}
 
 
-          
-          <div style={linkStyle}>
-            
-            <AboutMe />
-            <Portfolio />
-            <Contact />
-            <Resume />
-            <Footer />
 
-          </div>
-
-        </section>
-      </header>
-    );
-  }
-  
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,110 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
+
+
+export default function Foot() {
+    const linkStyle = { border: '1px black', padding: '5px' };
+  
+    return (
+      <header className="main-header-menu">
+        <section
+          style={{
+            display: 'flex',
+            fontFamily: 'helvetica',
+            flexDirection: 'row',
+            alignItems: 'flex-start',
+            justifyContent: 'flex-start',
+          }}
+        >
+          <div style={linkStyle}>
+            
+            <Footer />
+          </div>
+
+        </section>
+      </header>
+    );
+  }
+  
+
+
+
+
+
+// export default function Section() {
+//     const linkStyle = { border: '1px black', padding: '5px' };
+  
+//     return (
+//       <header className="main-header-menu">
+//         <section
+//           style={{
+//             display: 'flex',
+//             fontFamily: 'helvetica',
+//             flexDirection: 'row',
+//             alignItems: 'flex-start',
+//             justifyContent: 'flex-start',
+//           }}
+//         >
+
+
+          
+//           <div style={linkStyle}>
+            
+//             <AboutMe />
+//             <Portfolio />
+//             <Contact />
+//             <Resume />
+//             <Footer />
+
+//           </div>
+
+//         </section>
+//       </header>
+//     );
+//   }
+  
