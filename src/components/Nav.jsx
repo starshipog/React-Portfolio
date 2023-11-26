0bb07e4805068989257e442fae64a9dfd6dbe4e2
starshipog <chayewhitaker14@gmail.com>
2023-11-25 19:37:34 -0600

    fixed errors for portfolio links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -23,7 +23,7 @@ function NavTabs() {
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/AboutMe' ? 'nav-link active' : 'nav-link'}
         >
-          About
+          About Me
         </Link>
       </li>
       <li className="nav-item">
@@ -32,7 +32,7 @@ function NavTabs() {
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
         >
-          Blog
+          Portfolio
         </Link>
       </li>
       <li className="nav-item">
