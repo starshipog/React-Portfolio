701b5b9c0dd0e970fa99dd229df7ed3073e9e99a
starshipog <chayewhitaker14@gmail.com>
2023-11-25 20:51:50 -0600

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import Header from './components/Header';
 
 import Section from './components/Section';
--- src/main.jsx
+++ src/main.jsx
@@ -115,13 +115,13 @@ export default function Foot() {
 
 // 2) You should have a resume section that contains a link to download your resume as well as a list of your proficiences. (GET HELP ADDING LINK)
 
-// 3) Your links are not working for the different sections of your portfolio. When one of them is clicked, it should display a different page containing the necessary information. (ADD PROJECT 1, 2 & 3 THEN IM DONE)
+// 3) Your links are not working for the different sections of your portfolio. When one of them is clicked, it should display a different page containing the necessary information. (DONE)
 
-// 4) You are missing the required contact form. Make sure that you create a form for contacting you that contains form validation.
+// 4) You are missing the required contact form. Make sure that you create a form for contacting you that contains form validation. (DONE)
 
 // 5) Make sure that you are using conditional rendering to display the different pages of your application. Currently you are displaying all pages on a single large page. You should use React conditional rendering in order to display each page when the link is clicked on and hide the other pages. (DONE)
 
-// 6) Your commit history is a bit sparse. Make sure that you commit every time that you make a meaningful change to the code and give it a message that describes the change that was made. Think of commits as a history of the development of your repository.
+// 6) Your commit history is a bit sparse. Make sure that you commit every time that you make a meaningful change to the code and give it a message that describes the change that was made. Think of commits as a history of the development of your repository. (DONE)
 
 // 7) Make sure that you deploy this application. The deployment is a significant part of the grade, so get it deployed as soon as possible.
 
